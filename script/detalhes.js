79a0d12c44f8de49e79a15da6ae83d5c676d9af7
Rafael Correa <traytray.cfs@gmail.com>
2026-06-01 16:52:18 -0300

    Adiciona section habilidades

--- script/detalhes.js
+++ script/detalhes.js
@@ -4,7 +4,8 @@ const detalhes = {
     async mostrarCard() {
         const urlParams = new URLSearchParams(window.location.search)
         const idCartaBuscada = urlParams.get('id')
-        const inner = document.getElementById('card-info-details')
+        const innerCarta = document.getElementById('card-info-details')
+        const innerGridHabilidades = document.getElementById('habilidades-card-grid')
 
         if (!idCartaBuscada) {
             alert("Nenhuma carta foi selecionada.");
@@ -13,20 +14,26 @@ const detalhes = {
 
     try {
         const cartas = await api.buscarCartas()
+        const habilidades = await api.buscarHabilidadesDasCartas()
 
         const cartaEncontrada = cartas.find(carta => carta.id == idCartaBuscada || carta.nome == idCartaBuscada)
 
+        const habilidadesDaCartaEncontrada = habilidades.filter(habilidade => habilidade.cartaId == idCartaBuscada)
+
         if(cartaEncontrada){
-            detalhes.exibirDetalhes(cartaEncontrada, inner)
+            detalhes.exibirDetalhes(cartaEncontrada, innerCarta)
+            detalhes.exibirHabilidades(habilidadesDaCartaEncontrada, innerGridHabilidades)
         }
         else{
-            inner.innerHTML = `
+            innerCarta.innerHTML = `
                     <div class="alert alert-warning text-center w-100" role="alert">
                         <h4>Carta não encontrada!</h4>
                         <p>O ID ou nome informado não corresponde a nenhuma carta da coleção.</p>
                         <a href="../index.html" class="btn btn-warning mt-2">Voltar para a Home</a>
                     </div>
                 `;
+            
+            innerGridHabilidades.innerHTML = '';
         }
     } 
     catch (error) {
@@ -55,7 +62,25 @@ const detalhes = {
     `
 
     inner.appendChild(item)
-}
+},
+
+    exibirHabilidades(habilidades, inner){
+        habilidades.forEach((habilidade) => {
+        const item = document.createElement('div')
+            item.className = 'col d-flex justify-content-center'
+            item.innerHTML = `
+            <div class="habilidade-card">
+            <img src=".${habilidade.imagem}" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">${habilidade.nome}</h5>
+                <p class="card-text">${habilidade.descricao}</p>
+            </div>
+            </div>
+            `
+
+            inner.appendChild(item)
+    })
+    }
 
 }
 
